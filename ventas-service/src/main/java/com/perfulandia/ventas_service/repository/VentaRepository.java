b9c728fc137578a1c1fe5416b072e754289e1fd0
rmindboy <l.toledo@duocuc.cl>
2025-06-05 19:06:14 -0400

    eliminacion microservicio sin HATEOAS implementado

--- ventas-service/src/main/java/com/perfulandia/ventas_service/repository/VentaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.perfulandia.ventas_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.perfulandia.ventas_service.model.Venta;
-
-public interface VentaRepository extends JpaRepository<Venta, Long> {
-}
