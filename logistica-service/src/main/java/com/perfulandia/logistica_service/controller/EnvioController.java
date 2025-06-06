b41db685b51b761d58c6af2215c7f70c9680760b
skalibxr <hmarmolejo3@gmail.com>
2025-06-06 00:43:02 -0400

    ruta RestController versionada

--- logistica-service/src/main/java/com/perfulandia/logistica_service/controller/EnvioController.java
+++ logistica-service/src/main/java/com/perfulandia/logistica_service/controller/EnvioController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/envios")
+@RequestMapping("/api/v1/envios")
 public class EnvioController {
 
     private final EnvioRepository envioRepository;
